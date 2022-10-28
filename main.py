97e63f11bdc732c57e22e4adbec1632bfdd34d35
maclory73 <lory.carvajol997@gmail.com>
2022-10-28 09:27:58 +0200

    1.2

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
 
+# SCRAPING DU PREMIER PRODUIT LA PAGE N°1
 url = 'http://books.toscrape.com/'
 
-def get_soup(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+page = requests.get(url)
 
-    return soup.find_all('p', class_='price_color')
+soup = BeautifulSoup(page.content, 'html.parser')
 
-#return universal_product_code
+products = soup.findAll('article', class_='product_pod')
+# récupérer le titre du livre
+title = products[0].find('h3').find('a').get('title')
+# récupérer l'image du livre
+image = products[0].find(
+    'div', class_='image_container').find('img').get('src')
+# récupérer la note du livre
+rating = products[0].find('p', class_='star-rating')['class'][1]
+# récupérer le prix du livre
+price = products[0].find('div', class_='product_price').find(
+    'p', class_='price_color').text.strip()
+# récupérer le stock du livre
+stock = products[0].find('div', class_='product_price').find(
+    'p', class_='instock availability').text.strip()
+# récupérer le lien du livre
+link = products[0].find('h3').find('a').get('href')
 
-def get_product_name(soup):
-    return soup.find('h1').text
+print('Title: ', title)
+print('Image: ', image)
+print('Rating: ', rating)
+print('Price: ', price)
+print('Stock: ', stock)
+print('Link: ', link)
+
+     # stocké les données dans un fichier csv
+with open('books.csv', 'a') as csv_file:
+         writer = csv.writer(csv_file)
+         writer.writerow([title, image, rating, price, stock, link])
+
+print('---------------------------------------')
 
 
 
-    
-    
-    
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
  
  
- 
-
-
-
-
-    
-    return soup
-
-print (get_soup(url))
